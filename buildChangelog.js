d6e8ac506d1e2d3b91185efa96517de40a07cef9
Pedro Escumalha <pedrosimoes93@gmail.com>
2024-03-10 19:06:12 +0100

    Update buildChangelog.js

--- buildChangelog.js
+++ buildChangelog.js
@@ -31,6 +31,8 @@ type: ${type}
 ${process.env.CHANGELOG_DESCRIPTION}
 `;
 
+console.log('!!!!!!!!!!!!!', process.env.CHANGELOG_DESCRIPTION, process.env.CHANGELOG_ID, process.env.CHANGELOG_TITLE);
+
 const filePath = `changelogs/${process.env.CHANGELOG_ID}.md`;
 
 if (existsSync(filePath)) {
